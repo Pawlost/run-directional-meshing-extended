b1be02f586b6d9b82198c963732e8331e2b35601
Pavel Balusek <mr.p.balusek@gmail.com>
2025-11-16 20:01:33 +0100

    Add module for automated tests

---
+++ Plugins/RunDirectionalMeshingExtended/Source/RDMTests/Private/RDMTests.cpp
@@ -0,0 +1,17 @@
+﻿#include "RDMTests.h"
+
+#define LOCTEXT_NAMESPACE "FRDMTestsModule"
+
+void FRDMTestsModule::StartupModule()
+{
+    
+}
+
+void FRDMTestsModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FRDMTestsModule, RDMTests)
---
+++ Plugins/RunDirectionalMeshingExtended/Source/RDMTests/Private/Unit/RDMTests_Example.cpp
@@ -0,0 +1,13 @@
+﻿#include "Misc/AutomationTest.h"
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(
+	FRDMTestsExample,
+	"RDMTests.Meshing.Example",
+	EAutomationTestFlags::EditorContext | EAutomationTestFlags::EngineFilter
+);
+
+bool FRDMTestsExample::RunTest(const FString& Parameters)
+{
+	TestTrue(TEXT("Initial unit test"), true);
+	return true;
+}
