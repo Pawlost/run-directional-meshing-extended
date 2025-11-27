cd8bb788740516a12131ffec152cfd134196f654
Pavel Balusek <mr.p.balusek@gmail.com>
2025-11-27 09:22:16 +0100

    Add RDMMeshersTests module

---
+++ Plugins/RunDirectionalMeshingExtended/Source/RDMMeshersTests/Private/RDMMeshersTests.cpp
@@ -0,0 +1,17 @@
+﻿#include "RDMMeshersTests.h"
+
+#define LOCTEXT_NAMESPACE "FRDMMeshersTestsModule"
+
+void FRDMMeshersTestsModule::StartupModule()
+{
+    
+}
+
+void FRDMMeshersTestsModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FRDMMeshersTestsModule, RDMMeshersTests)
